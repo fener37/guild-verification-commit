1b550875eb2543c65304a56a15814a1cfd7b8351
fener37 <1974akkoca@gmail.com>
2026-03-05 16:10:00 +0300

    Fix database connection timeout

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:02:25.527Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T13:09:59.299Z
+Commit: Fix database connection timeout
