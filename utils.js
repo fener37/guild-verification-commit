60e5ab9f830416fbb9289a0ea7453fa2303dd171
fener37 <1974akkoca@gmail.com>
2026-03-10 06:14:23 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T13:09:59.299Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-10T03:14:22.440Z
+Commit: Resolve null pointer exception in user service
